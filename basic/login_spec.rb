a1c3bb3c9321f9eb9dc753245db4b229abbe7e30
Krzysztof Bober <krzysztof.bober@getbase.com>
2015-11-13 17:07:11 +0100

    initial commit

---
+++ basic/login_spec.rb
@@ -0,0 +1,9 @@
+require 'spec_helper'
+
+module Basic
+  describe "Login" do
+    it "does nothing" do
+      binding.pry
+    end
+  end
+end
---
+++ spec_helper.rb
@@ -0,0 +1,36 @@
+require 'page-object'
+require 'page-object/page_factory'
+require 'appium_lib'
+require 'pry'
+require 'require_all'
+
+require_rel 'support'
+
+RSpec.configure do |config|
+  config.include PageObject::PageFactory
+  config.include CapabilityBuilder
+
+  config.before(:all) do
+    config = {
+      caps: capabilities,
+      appium_lib: {
+        debug: ENV['DEBUG'].to_s.downcase != 'false',
+        wait: 0
+      }
+    }
+    @driver = Appium::Driver.new(config).start_driver
+  end
+
+  config.after(:each) do
+    # Step debugger
+    binding.pry if  ENV['PRY'] == 'all'
+    # Debug failing steps
+    binding.pry if (ENV['PRY'] == 'failing' && RSpec.current_example.exception)
+    # Capture screenshot if exception
+    @driver.save_screenshot (RSpec.current_example.description + ".png").gsub " ", "_" if (RSpec.current_example.exception && (ENV['SCREENSHOT'] == "true"))
+  end
+
+  config.after(:all) do
+    @driver.quit unless @driver.nil?
+  end
+end
