e7186969f915dd343ae2d54ddf0efef5808c9f52
Krzysztof Bober <krzysztof.bober@getbase.com>
2015-11-19 15:42:00 +0100

    removing unwanted module

--- spec/login_spec.rb
+++ spec/login_spec.rb
@@ -1,9 +1,7 @@
 require 'spec_helper'
 
-module Basic
-  describe "Login" do
-    it "does nothing" do
-      binding.pry
-    end
+describe "Login" do
+  it "does nothing" do
+    binding.pry
   end
 end
